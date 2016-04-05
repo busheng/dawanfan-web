62f5ced27e51cdc17b136bf5090581914ad80f1a
Busheng Lou <busheng@bushengs-mbp.dynamic.ucsd.edu>
2016-04-04 17:47:55 -0700

    fix

--- php/api/login_check.php
+++ php/api/login_check.php
@@ -10,16 +10,8 @@ require_once('config.php');
 	$password = $_GET['password'];
 	if($user->login($username,$password)){ 
 		$_SESSION['username'] = $username;
-		//$_SESSION['userid'] = $user->get_user_id($username);
-
-		//header('Location: http://localhost/~busheng/dawanfancopy/');
-		//exit;
 	    echo "lgoin_success";
 	} else {
 		echo "login_fail";
-		//$error[] = 'Wrong username or password or your account has not been activated.';
 	}
-
-//}//end if submit
-
 ?>
